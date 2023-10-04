1058c15b5de6f2ddf4755447a40baeca4fa49fc0
Sorok <Sorok@DESKTOP-C9GGTD7>
2023-10-04 16:31:58 +0200

    Добавьте файлы проекта.

---
+++ lab Soroka.D 3/Program.cs
@@ -0,0 +1,36 @@
+﻿//10 Вариант Высокий уровень
+
+//Console.Write("Введите x:");
+//double x = double.Parse(Console.ReadLine());
+//Console.Write("Введите y:");
+//double y = double.Parse(Console.ReadLine());
+//Console.WriteLine(y <= 1 && y >= x && y >= -x);
+
+//4 Вариант Высокий уровень
+
+//Console.Write("Введите число от 1 до 99:");
+//int x = int.Parse(Console.ReadLine());
+//if (x == 1)
+//{
+//    Console.WriteLine($"{x} копейка");
+//}
+//else if (x >= 2 && x <= 4)
+//{
+//    Console.WriteLine($"{x} копейки");
+//}
+//else if (x >= 5 && x <= 20)
+//{
+//    Console.WriteLine($"{x} копеек");
+//}
+//else if (x > 20 && x % 10 == 1)
+//{
+//    Console.WriteLine($"{x} копейка");
+//}
+//else if (x > 20 && (x % 10 == 2 || x % 10 == 3 || x % 10 == 4))
+//{
+//    Console.WriteLine($"{x} копейки");
+//}
+//else if (x > 20 && (x % 10 == 5 || x % 10 == 6 || x % 10 == 7 || x % 10 == 8 || x % 10 == 9 || x % 10 == 0))
+//{
+//    Console.WriteLine($"{x} копеек");
+//}
